607fda681eb4708a07704be162541cee3983ee0e
Your Name <your.email@example.com>
2025-01-20 09:38:31 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { Route,Routes } from "react-router-dom"
-import Transfer from "./components/Transfer"
-import Contact from "./components/Contact"
+import Navbar from "./Navbar"
+import Hero from "./Hero"
+import Background from "./Background"
+import Projects from "./Projects"
+import Skills from "./Skills"
+import Education from "./Education"
+import Contact from "./Contact"
+import Footer from "./Footer"
+//import About from "./About"
 
 const App = () => {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Transfer />} />
-      <Route path="/contact" element={<Contact/>} />
-    </Routes>
+    <Background/>
+      <Navbar/>
+      <Hero/>
+      <Projects/>
+      {/* <About/> */}
+      <Skills/>
+      <Education/>
+      <Contact/>
+      <Footer/>
     </>
   )
 }
